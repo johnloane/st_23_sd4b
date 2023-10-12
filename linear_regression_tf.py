d752c20263484d39fa879dbfaeb6a73e881415a8
John Loane <john.loane@dkit.ie>
2023-10-12 20:48:21 +0100

    Building our first models
    By hand
    And then using tensorflow
    pip install tensorflow

---
+++ linear_regression_tf.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow.keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+n_pts = 500
+np.random.seed(0)
+Xa = np.array([np.random.normal(13, 2, n_pts), np.random.normal(12, 2, n_pts)]).T
+Xb = np.array([np.random.normal(8, 2, n_pts), np.random.normal(6, 2, n_pts)]).T
+X = np.vstack((Xa, Xb))
+Y = np.matrix(np.append(np.zeros(n_pts), np.ones(n_pts))).T
+plt.scatter(X[:n_pts, 0], X[:n_pts, 1])
+plt.scatter(X[n_pts:, 0], X[n_pts:, 1])
+plt.show()
+
+model = Sequential()
+model.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
+adam = Adam(lr=0.1)
+model.compile(adam, loss='binary_crossentropy', metrics=['accuracy'])
+h = model.fit(x=X, y=Y, verbose=1, batch_size=50, epochs=500, shuffle='true')
+plt.plot(h.history['accuracy']) 
+plt.xlabel('epoch')
+plt.legend(['accuracy'])
+plt.title('accuracy')
+plt.show()
